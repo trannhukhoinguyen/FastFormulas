af0cde66700224c092d8779fd161d45a8f64f2e4
Kaedehara Kazuha <trannhukhoinguyen@gmail.com>
2023-06-30 15:01:57 +0700

    Update getTotalPrice.js

--- getTotalPrice.js
+++ getTotalPrice.js
@@ -1,11 +1,13 @@
-// This formula helps you calculate the total price from the given detailed prices (such as different ticket prices of adults and children...)
+// This formula helps you calculate the total price from the given detailed prices (such as different ticket prices of adults and children...) 
+// and given price policies (such as the coefficient between adult quantiy and ticket price decreasement for children)
 const getTotalPrice = (
-  sumAdult, // quantity of Adults
-  sumChild, // quantity of Children
-  priceAdult, // price of an Adult ticket
-  priceChild // price of a Child ticket
+  sumAdult, // quantity of Adults - type: Number
+  sumChild, // quantity of Children - type: Number
+  priceAdult, // price of an Adult ticket - type: Number
+  priceChild, // price of a Child ticket - type: Number 
+  coefficient, // coefficient from the price policy - type: Number
 ) => {
  sumAdult * priceAdult 
-  + [Math.abs(sumChild - Math.floor(sumAdult / 2)) + sumChild - Math.floor(sumAdult / 2)] * priceAdult / 2 
-  + [sumChild + Math.floor(sumAdult / 2) - Math.abs(sumChild - Math.floor(sumAdult / 2))] * Math.ceil(sumChild / (sumChild + 1)) * priceChild / 2
+  + [Math.abs(sumChild - Math.floor(sumAdult / coefficient)) + sumChild - Math.floor(sumAdult / coefficient)] * priceAdult / 2 
+  + [sumChild + Math.floor(sumAdult / coefficient) - Math.abs(sumChild - Math.floor(sumAdult / coefficient))] * Math.ceil(sumChild / (sumChild + 1)) * priceChild / 2
 }
