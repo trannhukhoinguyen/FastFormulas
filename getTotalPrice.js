8aaa6f913296275f4445176759230bbbbc79824d
Kaedehara Kazuha <trannhukhoinguyen@gmail.com>
2023-06-30 14:53:47 +0700

    Update getTotalPrice.js

--- getTotalPrice.js
+++ getTotalPrice.js
@@ -1,5 +1,11 @@
-const getTotalPrice = (Adult, child, priceAdult, priceChild) => {
-  Adult * priceAdult 
-  + [Math.abs(child - Math.floor(Adult / 2)) + child - Math.floor(Adult / 2)] * priceAdult / 2 
-  + [child + Math.floor(Adult / 2) - Math.abs(child - Math.floor(Adult / 2))] * Math.ceil(child / (child + 1)) * priceChild / 2
+// This formula helps you calculate the total price from the given detailed prices (such as different ticket prices of adults and children...)
+const getTotalPrice = (
+  sumAdult, // quantity of Adults
+  sumChild, // quantity of Children
+  priceAdult, // price of an Adult ticket
+  priceChild // price of a Child ticket
+) => {
+ sumAdult * priceAdult 
+  + [Math.abs(sumChild - Math.floor(sumAdult / 2)) + sumChild - Math.floor(sumAdult / 2)] * priceAdult / 2 
+  + [sumChild + Math.floor(sumAdult / 2) - Math.abs(sumChild - Math.floor(sumAdult / 2))] * Math.ceil(sumChild / (sumChild + 1)) * priceChild / 2
 }
