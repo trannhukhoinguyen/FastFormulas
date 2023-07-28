ddddf7ad8522e435e4d77b3510767b28a13637dd
Miguel <trannhukhoinguyen@gmail.com>
2023-07-28 16:36:54 +0700

    Update removeItemBeHated.js

--- removeItemBeHated.js
+++ removeItemBeHated.js
@@ -1,7 +1,7 @@
 import {ref} from 'vue';
 
 let checkedList = ref([]);
-const itemsHateTogether = ['Kiwi', 'Lemon', 'Guava'];
+const itemsHateTogether = ['hateOthers1', 'hateOthers2', 'hateOthers3', ...];
 export const removeItemBeHated = () => {
   if(checkedList.value.filter(item => itemsHateTogether.includes(item)).length >1) {      
     checkedList.value = checkedList.value.filter(item => !itemsHateTogether.includes(item)).concat(checkedList.value[checkedList.value.length-1])
