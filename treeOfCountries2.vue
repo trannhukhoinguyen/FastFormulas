d4d0ad0f93fa4d1a4f5cfeb933890192e1643dff
Miguel <trannhukhoinguyen@gmail.com>
2023-09-28 15:08:05 +0700

    Create treeOfCountries2.vue

---
+++ treeOfCountries2.vue
@@ -0,0 +1,44 @@
+<template> 
+  <div>
+    <a-tree 
+          v-model:selectedKeys="selectedKeys" 
+          :tree-data="treeData" 
+          show-icon
+      >
+      <template #switcherIcon="{ switcherCls }">
+        <DeploymentUnitOutlined :class="switcherCls" />
+      </template>
+      <template #icon="{ key, selected }">
+        <template v-if="key.split('-').length === 2">
+          {{countryList[key.slice(2)].flag}}
+        </template>
+      </template>
+    </a-tree>        
+  </div>   
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { DeploymentUnitOutlined } from '@ant-design/icons-vue';
+import { countries } from 'country-list-json';
+
+const countryList = countries;
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char =>  127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+};
+console.log('zzz', getFlagEmoji('ab'));
+
+const selectedKeys = ref(['0-0-0']);
+
+const treeData = countryList.map((nation, indexNation) => ({
+    title: nation.name + ' (' + nation.dial_code + ')',
+    key: `0-${indexNation}`,
+    children: [{
+      title: indexNation%2 ? 'blue' : 'green',
+      key: `0-${indexNation}-1`,
+    }],
+}));
+</script>
