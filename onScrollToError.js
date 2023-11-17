e0482f2b16819ff6bc37eb48cbd2774e97cc08e2
Miguel <trannhukhoinguyen@gmail.com>
2023-11-17 10:30:18 +0700

    Create onScrollToError.js

---
+++ onScrollToError.js
@@ -0,0 +1,20 @@
+export const onScrollToError = (errorInfo) => {
+  if (errorInfo.errorFields?.length > 0) {
+    const elementError =
+      typeof errorInfo.errorFields[0].name === 'string'
+        ? [errorInfo.errorFields[0].name]
+        : errorInfo.errorFields[0].name;
+    let idFirstErrorElement = 'form_';
+    elementError.forEach((err, i, arr) => {
+      if (arr.length > 1 && i !== arr.length - 1) {
+        idFirstErrorElement += ${err}_;
+      } else {
+        idFirstErrorElement += err;
+      }
+    });
+    const element = document.getElementById(idFirstErrorElement);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+};
