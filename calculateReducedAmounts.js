f66c95c6e055a828e60b5b61ea7c39eb3f2b4eda
Miguel <trannhukhoinguyen@gmail.com>
2023-12-18 14:00:45 +0700

    Update calculateReducedAmounts.js

--- calculateReducedAmounts.js
+++ calculateReducedAmounts.js
@@ -4,12 +4,30 @@ const calculateReducedAmounts = (
   totalBeforeReduce, // total Money before reducingn - type: Number (optional)
   totalReducedAmount, // total Reduced Amount - type: Number
   discountPercent, // percent of discount - type: Number (optional)
+  quantity1, // quantity of products 1 - type: Number
+  quantity2, // quantity of products 2 - type: Number (optional)
+  //...
 ) => {
-  let amounts = [];
+  let reducedAmount1;
+  let reducedAmount2;
   if(discountPercent) {
+    reducedAmount1 = discountPercent * quantity1;
+    reducedAmount2 = discountPercent * quantity2;
+  } else {
+    let discountPercentTemp;
     if(totalBeforeReduce) {
-      amounts = [];
+      discountPercentTemp = totalReducedAmount / totalBeforeReduce;
+    } else {
+      discountPercentTemp = totalReducedAmount / (totalReducedAmount + totalAfterReduce);
     }
+    reducedAmount1 = discountPercentTemp * quantity1;
+    reducedAmount2 = discountPercentTemp * quantity2;
   }
-  amounts = [];
+  return {
+    {
+      quantity1: reducedAmount1,
+      quantity2: reducedAmount2,
+      //...
+    }
+  };
 }
