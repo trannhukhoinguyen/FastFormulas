73bc94a52dc6dff730a128e27a801724291069b0
Miguel <trannhukhoinguyen@gmail.com>
2023-12-18 12:01:55 +0700

    Create calculateReducedAmounts.js

---
+++ calculateReducedAmounts.js
@@ -0,0 +1,9 @@
+// This formula helps you calculate the reduced amounts of a bill with many product and each people buying different quantities of products.
+const calculateReducedAmounts = (
+  totalAfterReduce, // total Money after reducing - type: Number
+  totalBeforeReduce, // total Money before reducingn - type: Number (optional)
+  totalReducedAmount, // total Reduced Amount - type: Number
+  discountPercent, // percent of discount - type: Number (optional)
+) => {
+
+}
