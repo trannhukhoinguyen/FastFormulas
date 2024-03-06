7ce7cca34e857597421818765eb59f1e5ae9d103
Nam Mô A Di Đà Phật <trannhukhoinguyen@gmail.com>
2024-03-06 17:08:55 +0700

    Create AutoScroll.js

---
+++ Puppeteer/AutoScroll.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.yoursite.com');
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+
+    await autoScroll(page);
+
+    await page.screenshot({
+        path: 'yoursite.png',
+        fullPage: true
+    });
+
+    await browser.close();
+})();
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight - window.innerHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+};
+
+// Source: https://github.com/chenxiaochun/blog/issues/38
